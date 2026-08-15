09d77c5ae82cc15a02d8ab47c59f6cb9329c61f9
jokerhj07 <manasvi.readymadegarment@gmail.com>
2026-08-15 21:04:02 +0530

    Add files via upload

---
+++ bot/config.py
@@ -0,0 +1,52 @@
+from os import environ as env
+
+class Telegram:
+    API_ID = int(env.get("TELEGRAM_API_ID", 21249497))
+    API_HASH = env.get("TELEGRAM_API_HASH", "6adf3d3cc76505f893fc61ca4666a592")
+    OWNER_ID = int(env.get("OWNER_ID", 6026244374))
+    ALLOWED_USER_IDS = env.get("ALLOWED_USER_IDS", "6026244374").split()
+    BOT_USERNAME = env.get("TELEGRAM_BOT_USERNAME", "HJFiletoLinkDoraemonShowBot")
+    BOT_TOKEN = env.get("TELEGRAM_BOT_TOKEN", "8865518119:AAFb3ouMZIKM8lNoLlkCetZ79d4jbGvzzxU")
+    CHANNEL_ID = int(env.get("TELEGRAM_CHANNEL_ID", -1003928829783))
+    SECRET_CODE_LENGTH = int(env.get("SECRET_CODE_LENGTH", 12))
+
+class Server:
+    BASE_URL = env.get("BASE_URL", "https://itshj-file-to-stream-bot-doremon-show.onrender.com/")
+    BIND_ADDRESS = env.get("BIND_ADDRESS", "0.0.0.0")
+    PORT = int(env.get("PORT", 8080))
+
+# LOGGING CONFIGURATION
+LOGGER_CONFIG_JSON = {
+    'version': 1,
+    'formatters': {
+        'default': {
+            'format': '[%(asctime)s][%(name)s][%(levelname)s] -> %(message)s',
+            'datefmt': '%d/%m/%Y %H:%M:%S'
+        },
+    },
+    'handlers': {
+        'file_handler': {
+            'class': 'logging.FileHandler',
+            'filename': 'event-log.txt',
+            'formatter': 'default'
+        },
+        'stream_handler': {
+            'class': 'logging.StreamHandler',
+            'formatter': 'default'
+        }
+    },
+    'loggers': {
+        'uvicorn': {
+            'level': 'INFO',
+            'handlers': ['file_handler', 'stream_handler']
+        },
+        'uvicorn.error': {
+            'level': 'WARNING',
+            'handlers': ['file_handler', 'stream_handler']
+        },
+        'bot': {
+            'level': 'INFO',
+            'handlers': ['file_handler', 'stream_handler']
+        }
+    }
+}
